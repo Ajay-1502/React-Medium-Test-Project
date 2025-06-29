b54f48a964d326baad3853932b8b65b9e341c0af
Ajay-1502 <ahdesai40@gmail.com>
2025-06-29 14:11:08 +0530

    Feature : Displays data on UI & integrated local storage for data persistance

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,22 +1,64 @@
+import { useState, useEffect } from 'react';
+
 const ProductDetails = () => {
+  const [products, setProducts] = useState(() => {
+    const storedProducts = localStorage.getItem('products');
+    return storedProducts ? JSON.parse(storedProducts) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('products', JSON.stringify(products));
+  }, [products]);
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    let productObj = {
+      id: Date.now(),
+      name: event.target.tshirt.value,
+      description: event.target.description.value,
+      price: event.target.price.value,
+      lSize: event.target.L.value,
+      mSize: event.target.M.value,
+      sSize: event.target.S.value,
+    };
+
+    setProducts((prevProducts) => [...prevProducts, productObj]);
+    event.target.reset();
+  };
+
   return (
-    <div>
-      <form>
-        <label htmlFor="tshirt">T-Shirt Name :</label>
-        <input id="tshirt" type="text" />
-        <label htmlFor="description">Description :</label>
-        <input id="description" type="text" />
-        <label htmlFor="price">Price :</label>
-        <input id="price" type="number" min="1" />
-        <label htmlFor="L-size">L</label>
-        <input id="L-size" min="1" type="number" />
-        <label htmlFor="M-size">M</label>
-        <input id="M-size" min="1" type="number" />
-        <label htmlFor="S-size">S</label>
-        <input id="S-size" min="1" type="number" />
-        <button>Add Product</button>
-      </form>
-    </div>
+    <>
+      <div>
+        <form onSubmit={formSubmitHandler}>
+          <label htmlFor="tshirt">T-Shirt Name :</label>
+          <input id="tshirt" name="tshirt" type="text" />
+          <label htmlFor="description">Description :</label>
+          <input id="description" name="description" type="text" />
+          <label htmlFor="price">Price :</label>
+          <input id="price" name="price" type="number" min="0" />
+          <label htmlFor="L-size">L</label>
+          <input id="L-size" name="L" min="0" type="number" />
+          <label htmlFor="M-size">M</label>
+          <input id="M-size" name="M" min="0" type="number" />
+          <label htmlFor="S-size">S</label>
+          <input id="S-size" name="S" min="0" type="number" />
+          <button type="submit">Add Product</button>
+        </form>
+      </div>
+      <div>
+        {products.map((product) => {
+          return (
+            <ul key={product.id}>
+              {product.name} - {product.description} -₹{product.price} -
+              <button>Buy Large {product.lSize}</button>
+              <button>Buy Medium {product.mSize}</button>
+              <button> Buy Small {product.sSize}</button>
+            </ul>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
